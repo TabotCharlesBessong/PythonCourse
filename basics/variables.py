c0d4061c32e65988927077c7077cfca4766e3462
Tabot Charles Bessong <ukujula.tech@gmail.com>
2022-02-22 16:33:41 +0100

    basics variables in python

---
+++ basics/variables.py
@@ -0,0 +1,13 @@
+firstName = 'Charles'
+lastName = 'Bessong Tabot'
+fullName = firstName + " "+ lastName
+# print("Hello "+ fullName)
+height = 172.75
+print(height)
+dog = False
+
+birthDate = 2002 
+presentDate = 2022 
+age = presentDate -  birthDate
+marriageAge = age + 3
+print("Hello "+ fullName + " you are "+  str(age) + " years old" + " you will get married at " + str(marriageAge) + " years of age"  )
