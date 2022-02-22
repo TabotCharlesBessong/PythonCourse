3af7a3c57a0da92e98c8a148c162e49f02ca8608
Tabot Charles Bessong <ukujula.tech@gmail.com>
2022-02-22 17:04:35 +0100

    getting users input

---
+++ basics/userInput.py
@@ -0,0 +1,22 @@
+# input('Whats your name dude?: ')
+firstName = input('Whats your first name?: ')
+lastName = input('Whats your last name?: ')
+print("hello "+ str(firstName))
+fullName = str(firstName) + " " + str(lastName)
+print("Hello "+ fullName)
+height = input('Whats your height in cm?: ')
+print(height)
+dog = False
+# 
+birthDate = input('Whats your birth year')
+presentDate = 2022 
+age = int(presentDate) - int(birthDate)
+marriageAge = int(age) + 3
+print("Hello "+ fullName + " you are "+  str(age) + " years old" + " you will get married at " + str(marriageAge) + " years of age"  )
+
+## multiple assignment , allows us to assign multiple of variables at the same time
+# name = 'Charles'
+# age1 = 24
+# attractive = False
+# name , age1 , attractive = 'Charles' ,24 , False
+# Charles  = Junior  = Tabot = Bessong = 20
