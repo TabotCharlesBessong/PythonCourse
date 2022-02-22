bb9f258b548d899fbcdf9d8f3a535eb511caf55e
Tabot Charles Bessong <ukujula.tech@gmail.com>
2022-02-22 17:55:01 +0100

    variables and functions

--- basics/basicMathModule.py
+++ basics/basicMathModule.py
@@ -2,5 +2,16 @@
 from cmath import pi
 
 
+import math
+
 pi = 3.14
 x = 1
+y = 2
+z = 3 
+
+print(round(pi))
+print(math.ceil(pi))
+print(abs(pi))
+print(pow(pi,x))
+print(math.sqrt(625))
+print(max(x,y,z))
--- basics/stringFunctions.py
+++ basics/stringFunctions.py
@@ -1,6 +1,12 @@
 
 name = "Charles"
+lasName = 'Bessong Tabot'
 
+fName = lasName[:7]
+lName = lasName[8:]
+funky = lasName[::2]
+reverName = lasName[::-1]
+print(lName + "\n" + funky + "\n" + reverName + "\n" + fName )
 print(len(name))
 print(name.find('s'))
 print(name.capitalize())
@@ -10,4 +16,16 @@ print(name.isdigit())
 print(name.isalpha())
 print(name.replace('e','i'))
 print(name*3)
+
+website = 'https://google.com'
+website2 = 'https://github.com/TabotCharlesBessong'
+
+slice = slice(7,-4)
+# slice[start:stop:step]
+
+print(website[slice])
+# print(website2[slice])
+web = website2[:6:3]
+web1 = website2[0::3]
+print(web1)
 
