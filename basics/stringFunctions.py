6f2951170a38282a8f20aa03ae906f36c6aa4438
Tabot Charles Bessong <ukujula.tech@gmail.com>
2022-02-22 16:46:52 +0100

    string functions

---
+++ basics/stringFunctions.py
@@ -0,0 +1,13 @@
+
+name = "Charles"
+
+print(len(name))
+print(name.find('s'))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.replace('e','i'))
+print(name*3)
+
--- basics/variables.py
+++ basics/variables.py
@@ -11,3 +11,10 @@ presentDate = 2022
 age = presentDate -  birthDate
 marriageAge = age + 3
 print("Hello "+ fullName + " you are "+  str(age) + " years old" + " you will get married at " + str(marriageAge) + " years of age"  )
+
+### multiple assignment , allows us to assign multiple of variables at the same time
+# name = 'Charles'
+# age1 = 24
+# attractive = False
+name , age1 , attractive = 'Charles' ,24 , False
+Charles  = Junior  = Tabot = Bessong = 20
