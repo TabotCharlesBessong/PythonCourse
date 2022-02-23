090cd74111647a48e60ffc619490ad6fd2fa7e33
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-02-23 12:51:16 +0100

    more on parameters

--- functions/simple.py
+++ functions/simple.py
@@ -1,5 +1,8 @@
 
 # simple
+# from turtle import title
+
+
 def hello(fName,lName,age):
   print('Hello '+ fName+ " "+lName)
   print('You are '+ str(age)+' years old')
@@ -19,10 +22,22 @@ def key (f,m,l):
 key(f='Python',m='Javascript',l='c++')
 
 def add(*stuffs):
+  # args pack all argument into a single one 
+  # args are tuple so unchangable so to change it , you need to make it a list 
   sum = 0
   stuffs = list(stuffs)
-  stuffs[0] = 0
+  # stuffs[0] = 0
   for i in stuffs:
     sum += i
   return sum
 print(add(1,2,3,4,5,6))
+
+# **kwargs paramenters will pack all argument into a dictionaries
+
+def kwack(**a):
+  print('Hello '+ a['first'] + " " + a['last'])
+  print('hello',end=" ")
+  for k,v in a.items():
+    print(v,end=" ")
+
+kwack(title="Mr .",first='Bro',middle="Dude",last="Code",other="Me")
