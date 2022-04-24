0724b02bfe6c913013eb221b3ab8246a7cc9c84a
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-24 20:52:34 +0100

    started working on our network

---
+++ LanNetwork/learning/basic.py
@@ -0,0 +1,7 @@
+# print('Hello world')
+import socket
+connexion_principal = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# lets set up our port 
+connexion_principal.bind(('',12800))
+# now we need to listen to different events
